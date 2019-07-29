65a64e75f1f098140eb3cd81d99f1f20183736a9
psaywan <39955892+psaywan@users.noreply.github.com>
2019-07-28 19:40:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "web" {
 
     connection {
       type = "ssh"
-      host = "${aws_instance.web.*.public_dns}"
+      host = "${aws_instance.web.*.public_ip}"
       user = "${var.ssh_user}"
       private_key = "${var.ssh_key}"
     }
